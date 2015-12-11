eec6263cad11f0a8678b2fe4f556caf94e159953
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2015-12-11 20:13:06 +0300

    Added some JsDoc

--- VendingMachineWeb/VendingMachineWeb/Scripts/VM/goods-store.js
+++ VendingMachineWeb/VendingMachineWeb/Scripts/VM/goods-store.js
@@ -1,12 +1,18 @@
-﻿define("goods-store", ["jquery", "purcashing-result", "knockoutjs"], function ($, purcashingResult, ko) {
+﻿/**
+ * goods-store module.
+ * @module goods-store
+ */
+define("goods-store", ["jquery", "purcashing-result", "knockoutjs"], function ($, purcashingResult, ko) {
     'use strict';
     
 
-    /**
-     *  Initializes new instance of GoodsStorage.
-     *  @constructor
+    /**     
+     *  Initializes new instance of goods storage.               
+     *  @memberof module:goods-store     
+     *  @constructs GoodsStore          
+     *  @classdesc Represents a storage of goods, keeping items count and price for each item.          
      *  @param {goodItem} goodItems The items in store, which are available for selling.
-     *  @wallet {Wallet} wallet Wallet for recieving money and giving change.
+     *  @param {Wallet} wallet Wallet for recieving money and giving change.
      */
     var thisClass = function(goodItems, wallet) {
 
@@ -20,14 +26,16 @@
 
         this.wallet = wallet;
     };
+        
+    thisClass.prototype = /** @lends module:goods-store.GoodsStore.prototype */ {
 
-    thisClass.prototype = {
-
-        /**
-         * Sells goods by name accepting an array of money units.
+        /**         
+         * Sells a goods item by name accepting an array of money units. If money amount is enough, diminishes goods item count and returns change. 
+         * If the money amount is exact, then the change is undefined.        
          * @param {string} name The name of goods.
          * @param {moneyItem[]} moneyItems An array of money items.
-         * @returns {PurcashingResult} operation status and change, if any. When the summ of moneyItems is exact or in case of an error, change is undefined.
+         * @returns {PurcashingResult} operation status and change, if any. Change is undefined when the summ of moneyItems is exact or in case of an error.                  
+         * @throws {Error} When the good item hasn't found by name 
          */
         purchase: function(name, moneyItems) {
             var item;
@@ -38,14 +46,14 @@
                 }
 
             if (!item)
-                throw new Error('Goods ' + name + ' is not found');
+                throw new Error('Goods item "' + name + '" is not found');
 
-            if (item.countInPieces() < 1)
-                return new purcashingResult(purcashingResult.prototype.RUN_OUT); //run out
+            if (item.countInPieces() < 1) //run out of items
+                return new purcashingResult(purcashingResult.prototype.RUN_OUT);
 
             var cashSumm = this.wallet.getBalance(moneyItems);
-            if (cashSumm < item.price)
-                return new purcashingResult(purcashingResult.prototype.NOT_ENOUGHT_MONEY); //not enough money
+            if (cashSumm < item.price) //not enough money
+                return new purcashingResult(purcashingResult.prototype.NOT_ENOUGHT_MONEY); 
 
             this.wallet.deposit(moneyItems);
 
@@ -54,9 +62,8 @@
 
                 change = this.wallet.adjustedWithdraw(cashSumm - item.price);
                 if ($.isNumeric(change)) {
-                    if (this.wallet.withdraw(moneyItems) !== true) //cancelling depositing
-                        throw new Error('Inconsistent wallet ' + this.name);
-                    return new purcashingResult(purcashingResult.prototype.NO_CHANGE, change); //can't change
+                    this.wallet.withdraw(moneyItems); //cancelling depositing
+                    return new purcashingResult(purcashingResult.prototype.NO_CHANGE, change); //can't return change due to a lack of apropriate coins
                 }
             }
 
--- VendingMachineWeb/VendingMachineWeb/Scripts/VM/purcashing-result.js
+++ VendingMachineWeb/VendingMachineWeb/Scripts/VM/purcashing-result.js
@@ -1,15 +1,43 @@
-﻿define("purcashing-result", [], function () {
+﻿/**
+ * purcashing-result module.
+ * @module purcashing-result
+ */
+define("purcashing-result", [], function () {
     'use strict';
     
+    /**     
+    *  Initializes new instance of purcashing result.               
+    *  @memberof module:purcashing-result
+    *  @constructs PurcashingResult
+    *  @classdesc Represents a result of a purchase.          
+    *  @param {number} status Indicates the purchase result, whether it is sucsesfull or not.
+    *  @param {moneyItem[]} change When the purchase is secsesfull, returns change. If change is not needed, then undefined.
+    */
     var thisClass = function (status, change) {
         this.status = status;
         this.change = change;
     }
 
+    /**
+     * @enum {number}
+     * @constant     
+     */
     thisClass.prototype = {
+        /**
+         * Run out of items.         
+         */
         RUN_OUT: 0,
+        /**
+         * The deposit is not enough to buy the goods item.         
+         */
         NOT_ENOUGHT_MONEY: -1,
+        /**
+         * There is a lack of apropriate coins for change.         
+         */
         NO_CHANGE: -2,
+        /**
+         * The purchase is sucsesfully completed.         
+         */
         SOLD_OK: 1
     };        
 
